c8b4fab3a328f73bceb5b03d05ae54ef52c900a9
Yeo Zhi Zheng <zhizhengy@hcahospicecare.org.sg>
2026-02-04 13:33:48 +0800

    Initial Commit

---
+++ Thesis/ThesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThesisApp.swift
+//  Thesis
+//
+//  Created by Zhi Zheng Yeo on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ThesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
